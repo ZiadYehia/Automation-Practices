21f2590a38a253f69f9f5bb2ddf7f893824edd84
‪ziad mohamed‬‏ yehia <ziadmohamedyehia123@gmail.com>
2025-10-21 22:09:31 +0300

    first commit

---
+++ src/main/java/BrowserActions.java
@@ -0,0 +1,91 @@
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+import java.util.Set;
+
+public class BrowserActions {
+    WebDriver driver;
+
+    @Test
+    public void testCase() {
+        driver = new EdgeDriver();
+        maximize();
+        navigateTo("https://www.booking.com/");
+        getCurrentUrl();
+        getTitle();
+        getWindowHandle();
+        getWindowHandles();
+    }
+
+    public void navigateTo(String url) {
+        driver.navigate().to(url);
+    }
+
+    public void navigateBack() {
+        driver.navigate().back();
+    }
+
+    public void navigateForward() {
+        driver.navigate().forward();
+    }
+
+    public void refresh() {
+        driver.navigate().refresh();
+    }
+
+    public void maximize() {
+        driver.manage().window().maximize();
+    }
+
+    public void setPosition(int x, int y) {
+        //Point point = new Point(100, 100);
+        driver.manage().window().setPosition(new Point(x, y));
+    }
+
+    public void setSize(int w, int h) {
+        driver.manage().window().setSize(new Dimension(w, h));
+    }
+
+    public void fullScreen() {
+        driver.manage().window().fullscreen();
+    }
+
+    public void getCurrentUrl() {
+        String url = driver.getCurrentUrl();
+        System.out.println("Current Url: " + url);
+    }
+
+    public void getTitle() {
+        String title = driver.getTitle();
+        System.out.println("Title: " + title);
+    }
+
+    public void getPageSource() {
+        String pageSource = driver.getPageSource();
+        System.out.println("Page Source: " + pageSource);
+    }
+
+    public void getWindowHandle() {
+        String windowHandle = driver.getWindowHandle();
+        System.out.println("Window Handle: " + windowHandle);
+    }
+
+    public void getWindowHandles() {
+        Set<String> windowHandles = driver.getWindowHandles();
+
+        for (String windowHandle : windowHandles) {
+            System.out.println("Tab :" + windowHandle);
+        }
+    }
+
+    public void close() {
+        driver.close();
+    }
+
+    public void quit() {
+        driver.quit();
+    }
+}
---
+++ src/main/java/ElementActions.java
@@ -0,0 +1,89 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+public class ElementActions {
+    final String url = "https://aa-practice-test-automation.vercel.app/";
+    String filePath = "src/main/resources/README.md";
+    WebDriver driver;
+    By username = By.id("inputUsername");
+    By password = By.cssSelector("[type='password']");
+    By loginButton = RelativeLocator.with(By.tagName("button")).below(password);
+    By uploadFilesTab = By.cssSelector("[href='uploadFile.html']");
+    By uploadFile = By.id("regularFileInput");
+
+
+    @Test(priority = 1)
+    public void startDriver() {
+        driver = new EdgeDriver();
+        maximize();
+        navigateTo(url);
+    }
+
+    @Test(priority = 2)
+    public void fieldsEnabled() {
+        Assert.assertTrue(isEnabled(username));
+        Assert.assertTrue(isEnabled(password));
+    }
+
+    @Test(priority = 3)
+    public void loginTC() {
+        typing(username, "admin");
+        typing(password, "admin");
+        clicking(loginButton);
+    }
+
+    @Test(priority = 4)
+    public void uploadFileTC() {
+        clicking(uploadFilesTab);
+        uploadFile(uploadFile, filePath);
+    }
+
+    public void navigateTo(String url) {
+        driver.navigate().to(url);
+    }
+
+    public void maximize() {
+        driver.manage().window().maximize();
+    }
+
+    public WebElement findLocator(By by) {
+        return driver.findElement(by);
+    }
+
+    public void clicking(By by) {
+        driver.findElement(by).click();
+    }
+
+    public void clear(By by) {
+        driver.findElement(by).clear();
+    }
+
+    public void typing(By by, String text) {
+        clear(by);
+        driver.findElement(by).sendKeys(text);
+    }
+
+    public void uploadFile(By by, String path) {
+        driver.findElement(by).sendKeys(System.getProperty("user.dir") + File.separator + path);
+    }
+
+    public Boolean isEnabled(By by) {
+        return driver.findElement(by).isEnabled();
+    }
+
+    public Boolean isDisplayed(By by) {
+        return driver.findElement(by).isDisplayed();
+    }
+
+    public String getText(By by) {
+        return driver.findElement(by).getText();
+    }
+
+}
