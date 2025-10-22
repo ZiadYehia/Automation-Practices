9734f8c256325c4104a129747ee40e2e6b25302f
ziad mohamed yehia <ziadmohamedyehia123@gmail.com>
2025-10-22 17:27:37 +0300

    4th commit

--- src/main/java/Waits.java
+++ src/main/java/Waits.java
@@ -1,6 +1,9 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -8,7 +11,7 @@ import java.time.Duration;
 
 public class Waits {
     WebDriver driver;
-    String url = "https://the-internet.herokuapp.com/dynamic_loading/2";
+    String url = "https://the-internet.herokuapp.com/dynamic_loading/1";
 
     By startButton = By.tagName("button");
     By hiddenText = By.cssSelector("#finish > h4");
@@ -23,8 +26,9 @@ public class Waits {
     @Test(priority = 2)
     public void implicitWait() {
         clicking(startButton);
-        implicitWait(5L);
-        Assert.assertEquals("");
+        explicitWaitVisibilityClickabilityOfElementUsingLamda(driver, hiddenText, 10L);
+        Assert.assertEquals(getText(hiddenText), "Hello World!");
+        System.out.println(getText(hiddenText));
     }
 
     public void maximize() {
@@ -43,7 +47,30 @@ public class Waits {
         driver.findElement(by).click();
     }
 
+    public String getText(By by) {
+        return driver.findElement(by).getText();
+    }
+
     public void implicitWait(Long sec) {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(sec));
     }
+
+    public void explicitWaitVisibilityOfElement(WebDriver driver, By by, Long sec) {
+        new WebDriverWait(driver, Duration.ofSeconds(sec))
+                .until(ExpectedConditions.visibilityOfElementLocated(by));
+    }
+
+    public void explicitWaitVisibilityOfElementUsingLamda(WebDriver driver, By by, Long sec) {
+        new WebDriverWait(driver, Duration.ofSeconds(sec))
+                .until(d -> driver.findElement(by).isDisplayed());
+    }
+
+    public void explicitWaitVisibilityClickabilityOfElementUsingLamda(WebDriver driver, By by, Long sec) {
+        new WebDriverWait(driver, Duration.ofSeconds(sec))
+                .until(d -> {
+                    WebElement element = driver.findElement(by);
+                    return element.isDisplayed() && element.isEnabled();
+                });
+    }
+
 }
